9367f0d8bd7d16b86eb5c13e546f3671bdc5c588
Inna666 <innok666@yandex.ru>
2019-11-19 20:13:34 +0300

    update basket_page

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -3,7 +3,7 @@ from .locators import BasketPageLocators
 
 class BasketPage(BasePage):
     def should_not_be_products_in_basket(self):
-        assert self.is_not_element_present(*BasketPageLocators.BASKET_FORM)
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_FORM), "Basket is not empty"
 
     def should_be_empty_basket(self):
-        assert self.is_element_present(*BasketPageLocators.BASKET_EMPTY_ITEM)
+        assert self.is_element_present(*BasketPageLocators.BASKET_EMPTY_ITEM), "Basket is not empty"
